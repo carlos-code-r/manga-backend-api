4fdf2d0102841a040e709dc14e707e6d0fa81a19
Carlos <189779392+carlos-code-r@users.noreply.github.com>
2026-02-08 22:30:46 +0100

    crear estructura inicial del proyecto

---
+++ src/main/java/com/manga/backend/MangaBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.manga.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/backend/MangaBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
