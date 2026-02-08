ac2d98475fb8ca20d8cd88a8e8378bbdf241cba0
Carlos <189779392+carlos-code-r@users.noreply.github.com>
2026-02-08 23:27:48 +0100

    Añadir DTO de Manga

---
+++ src/main/java/com/manga/backend/dto/MangaDto.java
@@ -0,0 +1,22 @@
+package com.manga.backend.dto;
+
+import java.time.LocalDate;
+
+import com.manga.backend.enums.EstadoManga;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MangaDto {
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String descripcion;
+    private EstadoManga estado;
+    private LocalDate fechaAlta;
+}
