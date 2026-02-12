7c8d64d0a7cd76f4b10adebd962b56bcfb0d363f
Carlos <189779392+carlos-code-r@users.noreply.github.com>
2026-02-13 00:27:10 +0100

    Añadir CapituloRepository

---
+++ src/main/java/com/manga/backend/repository/CapituloRepository.java
@@ -0,0 +1,9 @@
+package com.manga.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manga.backend.model.Capitulo;
+
+public interface CapituloRepository extends JpaRepository <Capitulo,Long>{
+
+}
