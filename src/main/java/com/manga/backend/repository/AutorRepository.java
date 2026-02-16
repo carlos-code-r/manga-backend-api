5f926c52917de189d16b15e48337973a1d085e8e
Carlos <189779392+carlos-code-r@users.noreply.github.com>
2026-02-16 21:50:38 +0100

    Añadir AutorRepository

---
+++ src/main/java/com/manga/backend/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.manga.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manga.backend.model.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor,Long>{
+
+}
