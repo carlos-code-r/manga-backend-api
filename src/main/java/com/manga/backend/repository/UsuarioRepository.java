16d97fd6f519f35eb6d842df800e20f5bcf2db8b
Carlos <189779392+carlos-code-r@users.noreply.github.com>
2026-02-16 19:41:02 +0100

    Añadir UsuarioRepository

---
+++ src/main/java/com/manga/backend/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.manga.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manga.backend.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository <Usuario,Long> {
+
+}
